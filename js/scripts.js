82f7179b7218d005fb77f7702691fcf91cbbc56a
Chris Davis <chris.davis5440@gmail.com>
2017-01-23 15:34:26 -0500

    Added bootstrap framework

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,23 @@ $(function() {
 
 	var $dataEntered = $(".dataEntered");
 
+	/* Warning button close
+	$(".warning a").on("click", function() {
+		$(".warning").fadeOut(500);
+	});
+	*/
+
 	$("#submitData").on("click", function() {
 		//Make sure input fields are not left blank
 		if($dayInput.val() === "" || $timeInput.val() === "" || $distanceInput.val() === "" || $caloriesInput.val() === "") {
 			//Display please fill in all text fields warning
-			$(".warning").fadeIn(500).delay(1000).fadeOut(500);
+			$(".warning").fadeIn(500).delay(2000).fadeOut(500);
 		} else {
 			//Create ul with data
 			var $ulWork = $("<ul class'work'>" +
 					"<li><strong>Day:</strong> " + $dayInput.val() + "</li>" +
-					"<li><strong>Time Spent:</strong> " + $timeInput.val() + "</li>" +
-					"<li><strong>Distance:</strong> " + $distanceInput.val() + "</li>" +
+					"<li><strong>Time Spent:</strong> " + $timeInput.val() + " minutes</li>" +
+					"<li><strong>Distance:</strong> " + $distanceInput.val() + " miles</li>" +
 					"<li><strong>Calories Burned:</strong> " + $caloriesInput.val() + "</li>" +
 					"</ul>");
 			//Append ul with data to page
