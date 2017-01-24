8e77a630d284414997096f85e94894a6fbfd6c79
Chris Davis <chris.davis5440@gmail.com>
2017-01-24 11:41:40 -0500

    Updated warning box to display successful data entries

--- js/scripts.js
+++ js/scripts.js
@@ -40,11 +40,12 @@ $(function() {
 		//Make sure input fields are not left blank
 		if($dayInput.val() === "" || $timeInput.val() === "" || $distanceInput.val() === "" || $caloriesInput.val() === "") {
 			//Display please fill in only warning
-			$(".warning h2").text("Please complete entire form").closest(".warning").fadeIn(500).delay(2000).fadeOut(500);
+			$(".warning h2").text("Please complete entire form").closest(".warning").removeClass("bg-primary").addClass("alert-danger").addClass("bg-primary").fadeIn(500).delay(2000).fadeOut(500);
 		}else if(isNaN($timeInput.val()) || isNaN($distanceInput.val()) || isNaN($caloriesInput.val())) {
 			//Display please fill in only numbers warning
-			$(".warning h2").text("Please only input numbers").closest(".warning").fadeIn(500).delay(2000).fadeOut(500);
+			$(".warning h2").text("Please only input numbers").closest(".warning").removeClass("bg-primary").addClass("alert-danger").fadeIn(500).delay(2000).fadeOut(500);
 		} else {
+			$(".warning h2").text("Data Successfully Added!").closest(".warning").removeClass("alert-danger").addClass("bg-primary").fadeIn(500).delay(2000).fadeOut(500);
 			//Convert text input into number then push data into arrays
 			$timeData.push(parseFloat($timeInput.val()));
 			$distanceData.push(parseFloat($distanceInput.val()));
