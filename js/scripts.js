6c9a5102984a0cec376e7237e830e026f90a0994
Chris Davis <chris.davis5440@gmail.com>
2017-01-24 09:42:34 -0500

    Created data arrays for each input to output the total work

--- js/scripts.js
+++ js/scripts.js
@@ -17,26 +17,50 @@ $(function() {
 	});
 	*/
 
+	var $timeData = [];
+	var $distanceData = [];
+	var $caloriesData = [];
+
+	//Add array data function
+	function sumArray(array) {
+	  var total = 0;
+		for (i = 0; i < array.length; i++) {
+    	total += array[i];
+		}
+		return total;
+	}
+
+
 	$("#submitData").on("click", function() {
 		//Make sure input fields are not left blank
 		if($dayInput.val() === "" || $timeInput.val() === "" || $distanceInput.val() === "" || $caloriesInput.val() === "") {
 			//Display please fill in all text fields warning
 			$(".warning").fadeIn(500).delay(2000).fadeOut(500);
 		} else {
+			//Convert text input into number then push data into arrays
+			$timeData.push(parseFloat($timeInput.val()));
+			$distanceData.push(parseFloat($distanceInput.val()));
+			$caloriesData.push(parseInt($caloriesInput.val()));
 			//Create ul with data
-			var $ulWork = $("<ul class'work'>" +
+			var $ulWork = $("<ul class='work'>" +
 					"<li><strong>Day:</strong> " + $dayInput.val() + "</li>" +
-					"<li><strong>Time Spent:</strong> " + $timeInput.val() + " minutes</li>" +
-					"<li><strong>Distance:</strong> " + $distanceInput.val() + " miles</li>" +
-					"<li><strong>Calories Burned:</strong> " + $caloriesInput.val() + "</li>" +
+					"<li><strong>Time Spent:</strong> " + $timeData[$timeData.length - 1] + " minutes</li>" +
+					"<li><strong>Distance:</strong> " + $distanceData[$distanceData.length - 1] + " miles</li>" +
+					"<li><strong>Calories Burned:</strong> " + $caloriesData[$caloriesData.length - 1] + "</li>" +
 					"</ul>");
 			//Append ul with data to page
-		  	$(".dataEntered").append($ulWork);
-		  	//Clear input values for new entry
-		  	$dayInput.val("");
-		  	$timeInput.val("");
-		  	$distanceInput.val("");
-		  	$caloriesInput.val("");
+	  	$(".dataPopulation").append($ulWork);
+	  	//Calculate totalWork and update page
+	  	$(".timeTotal").text(sumArray($timeData));
+	  	$(".distanceTotal").text(sumArray($distanceData));
+	  	$(".caloriesTotal").text(sumArray($caloriesData));
+	  	//Testing Purposes
+	  	console.log($timeData, $distanceData, $caloriesData);
+	  	//Clear input values for new entry
+	  	$dayInput.val("");
+	  	$timeInput.val("");
+	  	$distanceInput.val("");
+	  	$caloriesInput.val("");
 		}
 		
 
